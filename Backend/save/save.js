470dd41bde8a076a60bd15abd81184b7e1f2cd26
Maxwell Smith <64322148+borksson@users.noreply.github.com>
2023-03-25 06:51:01 -0600

    Added save api call

--- Backend/save/save.js
+++ Backend/save/save.js
@@ -12,7 +12,7 @@ const DB_COLLECTION = "elements";
 //     "url": "https://www.testsite.com/page.html",
 //     "uuid": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
 //     "xpath": "/html/body/p",
-//     "inner_html": "This is a test paragraph" // This is not needed but could be nice
+//     "inner_html": "This is a test paragraph"
 // }
 
 const save = async (req, res) => {
