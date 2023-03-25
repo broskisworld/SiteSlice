e830d4b3725c43c4f66bd03358dfab0eb768a621
Maxwell Smith <64322148+borksson@users.noreply.github.com>
2023-03-25 13:14:00 -0600

    FTP is working!

--- Backend/proxy/tmp/README.md
+++
@@ -1,1 +0,0 @@
-This is the proxy tmp folder.
--- Backend/save/save.js
+++ Backend/save/save.js
@@ -17,8 +17,8 @@ const DB_COLLECTION = "elements";
 //     "inner_html": "This is a test paragraph"
 // }
 
-const FTP_FILE_PATH = "tmp/";
-const FTP_FILE_NAME = "page.html";
+const FTP_FILE_PATH = "/public_html/index-basic.html";
+const FTP_FILE_NAME = "index-basic.html";
 
 const save = async (req, res) => {
 
@@ -31,85 +31,81 @@ const save = async (req, res) => {
     }
 
     // Version 4, Login via FTP
+
+    let file;
     try {
-        ftp.getFile(FTP_FILE_PATH, FTP_FILE_NAME,req.body.ftp_username, req.body.ftp_password, req.body.ftp_host, req.body.ftp_port);
+        ftp.getFile(FTP_FILE_PATH, FTP_FILE_NAME, req.body.ftp_username, req.body.ftp_password, req.body.ftp_host, req.body.ftp_port, async () => {
+            file = fs.readFileSync("save/tmp/" + FTP_FILE_NAME);
+
+            // Access database
+            let db;
+            let collection;
+
+            try {
+                db = client.db(DB_NAME);
+                collection = db.collection(DB_COLLECTION);
+            } catch (err) {
+                return res.status(500).json({ message: "Error connecting to database: " + err.toString()});
+            }
+
+            // Iterate through the changes
+
+            let doc;
+
+            try {
+                doc = new dom().parseFromString(file.toString());
+                
+                for(let i=0; i < req.body.changes.length; i++) {
+                    let change = req.body.changes[i];
+                    // For each change, get the xpath from the db given the uuid
+
+                    let query = { uuid: change.uuid };
+                    let result = await collection.findOne(query);
+                    let element_xpath = result.xpath;
+
+                    // Update the file via the xpath to reflect the change
+                    xpath.select(element_xpath, doc)[0].firstChild.data = change.new_inner_html;
+                }
+
+            } catch (err) {
+                return res.status(500).json({ message: "Error updating element: " + err.toString()});
+            }
+
+            // Save the file
+            try {
+                fs.writeFileSync("save/tmp/" + FTP_FILE_NAME, doc.toString());
+            } catch (err) {
+                return res.status(500).json({ message: "Error writing file: " + err.toString()});
+            }
+
+            // Version 4, Upload the file via FTP
+            try {
+                ftp.uploadFile(FTP_FILE_PATH, "save/tmp/" + FTP_FILE_NAME,req.body.ftp_username, req.body.ftp_password, req.body.ftp_host, req.body.ftp_port, () => {
+                    // Clean tmp folder
+
+                    const directory = "save/tmp";
+
+                    fs.readdir(directory, (err, files) => {
+                    if (err) throw err;
+
+                    for (const file of files) {
+                        fs.unlink(path.join(directory, file), (err) => {
+                        if (err) throw err;
+                        });
+                    }
+                    });
+
+
+                    return res.status(200).json({ message: "Success" });
+                });
+            } catch (err) {
+                return res.status(400).json({ message: "Error uploading file: " + err.toString()});
+            }
+
+        });
     } catch (err) {
         return res.status(400).json({ message: "Error getting file: " + err.toString()});
     }
-    // Version 2, Look for the file locally based on the url passed in
-
-    let file;
-
-    try{
-        file = fs.readFileSync("tmp/" + FTP_FILE_NAME, 'utf8');
-    } catch (err) {
-        return res.status(400).json({ message: "File tmp/" + FTP_FILE_NAME + " does not exist."});
-    }
-
-    // Access database
-    let db;
-    let collection;
-
-    try {
-        db = client.db(DB_NAME);
-        collection = db.collection(DB_COLLECTION);
-    } catch (err) {
-        return res.status(500).json({ message: "Error connecting to database: " + err.toString()});
-    }
-
-    // Iterate through the changes
-
-    let doc;
-
-    try {
-        doc = new dom().parseFromString(file);
-        
-        for(let i=0; i < req.body.changes.length; i++) {
-            let change = req.body.changes[i];
-            // For each change, get the xpath from the db given the uuid
-
-            let query = { uuid: change.uuid };
-            let result = await collection.findOne(query);
-            let element_xpath = result.xpath;
-            
-            // Update the file via the xpath to reflect the change
-            xpath.select(element_xpath, doc)[0].firstChild.data = change.new_inner_html;
-        }
-
-    } catch (err) {
-        return res.status(500).json({ message: "Error updating element: " + err.toString()});
-    }
-
-    // Save the file
-    try {
-        fs.writeFileSync("tmp/" + FTP_FILE_NAME, doc.toString());
-    } catch (err) {
-        return res.status(500).json({ message: "Error writing file: " + err.toString()});
-    }
-
-    // Version 4, Upload the file via FTP
-    try {
-        ftp.uploadFile(FTP_FILE_PATH, "tmp/" + FTP_FILE_NAME,req.body.ftp_username, req.body.ftp_password, req.body.ftp_host, req.body.ftp_port);
-    } catch (err) {
-        return res.status(400).json({ message: "Error uploading file: " + err.toString()});
-    }
-
-    // Clean tmp folder
-
-    // const directory = "tmp";
-
-    // fs.readdir(directory, (err, files) => {
-    // if (err) throw err;
-
-    // for (const file of files) {
-    //     fs.unlink(path.join(directory, file), (err) => {
-    //     if (err) throw err;
-    //     });
-    // }
-    // });
-
-
-    return res.status(200).json({ message: "Success" });
 };
 
 module.exports = { 
