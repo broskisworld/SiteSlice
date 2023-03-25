b059e19a46808dd0f76771031cf332f78e6c207a
Chayse Thompson <a02277726@usu.edu>
2023-03-25 01:27:34 -0600

    home styles and slice animation

---
+++ Frontend/src/components/TextCycleInput.jsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+
+const TextCycleInput = ({ texts }) => {
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIndex((index) => (index + 1) % texts.length);
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [texts]);
+
+  return (
+    <div className="max-w-xl w-full">
+      <input 
+      className="cycle-input rounded-md border-2 border-gray-200 h-10 max-w-2xl w-full p-4 text-gray-400 transition focus:border-orange-300"
+      type="text" placeholder={texts[index]} />
+      <style>{`
+        .cycle-input::placeholder {
+          animation: fade-in-out 3s ease-in-out infinite;
+          -webkit-animation: fade-in-out 3s ease-in-out infinite;
+        }
+
+        @keyframes fade-in-out {
+          0% {
+            color: white;
+            opacity: 0;
+          }
+          50% {
+            color: black;
+            opacity: 1;
+          }
+          100% {
+            color: white;
+            opacity: 0;
+          }
+        }
+      `}</style>
+    </div>
+    
+  );
+};
+
+export default TextCycleInput;
