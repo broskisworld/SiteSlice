11522ba3b1a0000e2d96b4127d04e26eb50b120e
Maxwell Smith <64322148+borksson@users.noreply.github.com>
2023-03-24 20:54:05 -0600

    Added the proxy and save routes

--- Backend/save/save_routes.js
+++ Backend/save/save_routes.js
@@ -1,3 +1,10 @@
+// Import router
 const router = require('express').Router();
 
+// Create the save route
+router.post('/', (req, res) => {
+    res.send('NOT IMPLEMENTED: Save POST route' + req.body);
+});
+
+// Export the router
 module.exports = router;
