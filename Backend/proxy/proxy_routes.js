5127c61faca71f7811adbdc4838f5fe2a923d72c
Maxwell Smith <64322148+borksson@users.noreply.github.com>
2023-03-24 21:09:55 -0600

    Added proxy routes for the backend. The proxy routes are used to route the requests to the backend server. The proxy routes are added in the proxy_routes.js file.

--- Backend/proxy/proxy_routes.js
+++ Backend/proxy/proxy_routes.js
@@ -4,6 +4,13 @@ const router = require('express').Router();
 // Create the proxy route
 // TODO: add http regex
 router.get(/^.*$/, (req, res) => {
+    // Download url files
+
+    // UUID elements
+
+    // Proxy links
+
+    // Add Injectables.js
     res.send('NOT IMPLEMENTED: Proxy GET route.\nParams sent: ' + req.url);
 });
 
