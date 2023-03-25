b7b2735f695d57aa1f92f50c4d16b091c7c3b49c
Chayse Thompson <a02277726@usu.edu>
2023-03-24 21:54:05 -0600

    hopefully fixed tailwind

--- Frontend/src/pages/Editor.jsx
+++ Frontend/src/pages/Editor.jsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function Home() {
+export default function Editor() {
   return (
     <>
       <h1>Editor</h1>
       <Link to="/">Home</Link>
+      <div></div>
+      <iframe src="https://www.mixhers.com"></iframe>
     </>
   );
 }
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function Home() {
   return (
     <>
-      <h1>Home page</h1>
+      <h1 className="underline">Home page</h1>
       <Link to="/editor">Editor</Link>
     </>
   );
