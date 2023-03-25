89b502591824a24a0cd744d682d0e68cace6a73b
Maxwell Smith <64322148+borksson@users.noreply.github.com>
2023-03-25 01:17:04 -0600

    Step 8 is there baby

--- Backend/save/save.js
+++ Backend/save/save.js
@@ -2,17 +2,21 @@ const { validationResult } = require('express-validator');
 const fs = require('fs');
 const xpath = require('xpath');
 const dom = require('xmldom').DOMParser;
+const client = require('../db/db').client;
 
+const DB_NAME = "SiteSliceDB";
+const DB_COLLECTION = "elements";
 
-const TESTBENCH_PATH = '/Users/borkson/Code/HackUSU/SiteSlice/Frontend/Testbench/Basic-site/index.html';
-const TEST_XPATH_H1 = '/html/body/h1';
-const TEST_XPATH_P = '/html/body/p';
+// Test event
+// {
+//     "url": "https://www.testsite.com/page.html",
+//     "uuid": "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
+//     "xpath": "/html/body/p",
+//     "inner_html": "This is a test paragraph" // This is not needed but could be nice
+// }
 
-const update_element_xpath = (doc, element_xpath, new_inner_html) => {
-    xpath.select(element_xpath, doc)[0].firstChild.data = new_inner_html;
-};
+const save = async (req, res) => {
 
-const save = (req, res) => {
     // Validate the request
 
     const errors = validationResult(req);
@@ -21,40 +25,60 @@ const save = (req, res) => {
         return res.status(422).json({ errors: errors.array() });
     }
 
-    // TODO: Step 5, Build xpath update function
-    
-    update_element_xpath(TEST_XPATH_H1, 'Lick my tits');
-
     // Version 4, Login via FTP
 
-    // Look for the file locally based on the url passed in
+    // Version 2, Look for the file locally based on the url passed in
 
     let file;
 
     try{
-        file = fs.readFileSync(req.body.url, 'utf8')
+        file = fs.readFileSync(req.body.url, 'utf8');
     } catch (err) {
-        res.status(400).json({ message: "File " + req.body.url + " does not exist."})
+        return res.status(400).json({ message: "File " + req.body.url + " does not exist."});
+    }
+
+    // Access database
+    let db;
+    let collection;
+
+    try {
+        db = client.db(DB_NAME);
+        collection = db.collection(DB_COLLECTION);
+    } catch (err) {
+        return res.status(500).json({ message: "Error connecting to database: " + err.toString()});
     }
 
     // Iterate through the changes
 
-    let doc = new dom().parseFromString(file);
-    
-    for(let i=0; i < req.body.changes; i++) {
-        let change = req.body.changes;
-        // TODO: Add mongo connect
-        let xpath = TEST_XPATH_H1
-        update_element_xpath(doc, xpath, change.new_inner_html);
+    let doc;
+
+    try {
+        doc = new dom().parseFromString(file);
+        
+        for(let i=0; i < req.body.changes.length; i++) {
+            let change = req.body.changes[i];
+            // For each change, get the xpath from the db given the uuid
+
+            let query = { uuid: change.uuid };
+            let result = await collection.findOne(query);
+            let element_xpath = result.xpath;
+            
+            // Update the file via the xpath to reflect the change
+            xpath.select(element_xpath, doc)[0].firstChild.data = change.new_inner_html;
+        }
+
+    } catch (err) {
+        return res.status(500).json({ message: "Error updating element: " + err.toString()});
     }
 
-    console.log(doc.to)
+    // Save the file
+    try {
+        fs.writeFileSync(req.body.url, doc.toString());
+    } catch (err) {
+        return res.status(500).json({ message: "Error writing file: " + err.toString()});
+    }
 
-    // For each change, get the xpath from the db given the uuid
-
-    // Update the file via the xpath to reflect the change
-
-    res.send('NOT IMPLEMENTED: Save POST route ' + JSON.stringify(req.body));
+    return res.status(200).json({ message: "Success" });
 };
 
 module.exports = { 
