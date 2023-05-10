2cc6bab5d751f86a355e53104924a0a0868e24b3
Josh Bosley <broskisworld@gmail.com>
2023-05-09 23:29:25 -0600

    filter children by only real elements

--- Testing/index.js
+++ Testing/index.js
@@ -1,10 +1,18 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
 
+// INPUT
+let file = fs.readFileSync('./output.html');
+let file_contents = file.toString();
+let $ = cheerio.load(file_contents);
+console.log(`${file_contents.length} characters read from to index.html & parsed to cheerio`);
+console.log(`${$.root().html().length} characters in cheerio root`);
+
+// UTILS
 function get_css_path(el) {
     let path = [], parent;
-    while (parent = el.parentNode) {
-        path.unshift(`${el.tagName}:nth(${[].indexOf.call(parent.children, el)+1})`);
+    while (parent = el.parent) {
+        path.unshift(`${el.tagName}:nth-child(${parent.children.filter((node) => node.type == 'tag').indexOf(el) + 1})`);
         el = parent;
     }
     return `${path.join(' > ')}`.toLowerCase();
@@ -13,13 +21,15 @@ function get_css_path(el) {
 function update_element_in_html_by_css_query(css_query,html, new_inner_html) {
 }
 
-// INPUT
-let file = fs.readFileSync('../Frontend/Testbench/Lundahl-backup/homedir/public_html/index.html');
-let file_contents = file.toString();
-let $ = cheerio.load(file_contents);
-
 // PROCESSING
+let footer_col_1 = $('#bottom1')[0];
+let footer_col_1_css_path = get_css_path(footer_col_1);
+console.log(`footer_col_1: ${footer_col_1_css_path}`);
+let found_el = $(footer_col_1_css_path);
+console.log(`found_el: ${found_el.length}`);
+console.log(`Processing complete!`);
 
 // OUTPUT
 let output = $.root().html();
-fs.writeFileSync('./output.html', output);
+fs.writeFileSync('./output2.html', output);
+console.log(`${output.length} characters written to output2.html`);
